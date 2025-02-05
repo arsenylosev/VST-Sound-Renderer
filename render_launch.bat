5a14884adb349cad06d9f0cf3ce8922f06101fd7
Arseniy Losev <92808183+arsenylosev@users.noreply.github.com>
2025-02-05 08:42:40 +0000

    BREAKING CHANGE: add a script for cloud upload

--- render_launch.bat
+++ render_launch.bat
@@ -1,15 +1,9 @@
-@echo off
-for /d %%B in (D:\Serum_Render\Data\Batch_4_presets\*) do (
-    python render_wav.py ^
-    --plugin "C:\Program Files\Xfer\Serum\Serum_x64.dll" ^
-    --note-duration 1.5 ^
-    --render-duration 3 ^
-    --pitch-low 12 ^
-    --pitch-step 12 ^
-    --pitch-high 84 ^
-    --preset-dir "%%B" ^
-    --output-dir "C:\Temp_Storage"
-
-    python dropbox_upload.py --yes Batch_4 C:\Temp_Storage
-    for /r %%f in (C:\Temp_Storage\*) do rm %%f
-)
+python render_wav.py ^
+--plugin "C:\Program Files\Xfer\Serum\Serum_x64.dll" ^
+--note-duration 1.5 ^
+--render-duration 3 ^
+--pitch-low 12 ^
+--pitch-step 12 ^
+--pitch-high 84 ^
+--preset-dir "D:\Serum_Render\Data\Batch_4_presets\001" ^
+--output-dir "D:\Serum_Render\Data\Batch_4_renders"
